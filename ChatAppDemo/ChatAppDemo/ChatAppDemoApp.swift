66951a3636cb7b57e8e56516ff1966ee4667854f
priyanka.m3 <priyanka.m3@glowtouch.com>
2024-06-21 18:11:46 +0530

    ChatAppDemo BR : 1 First Commit

---
+++ ChatAppDemo/ChatAppDemo/ChatAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppDemoApp.swift
+//  ChatAppDemo
+//
+//  Created by Priyanka Mathur on 21/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
